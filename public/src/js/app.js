37ddfdfbb1bc61f9a549fb2420bf4b01552fa008
Lunik <guillaume.lunik@gmail.com>
2016-01-19 18:24:06 +0100

    include local storage module

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -3,16 +3,18 @@ $(document).ready(init)
 var socket = io()
 
 function init() {
-  $.getScript('src/js/socket.js', function() {
-    $.getScript('src/js/file.js', function() {
-      $.getScript('src/js/torrent.js', function() {
-        $.getScript('src/js/directory.js', function() {
-          $.getScript('src/js/list.js', function() {
-            $.getScript('src/js/mediainfo.js', function() {
-              $.getScript('src/js/event.js', function() {
-                $.getScript('src/js/pnotif/pnotif.js', function() {
-                  $.getScript('src/js/popup/popup.js', function() {
-                    socket.emit('ready')
+  $.getScript('src/js/storage.js', function() {
+    $.getScript('src/js/socket.js', function() {
+      $.getScript('src/js/file.js', function() {
+        $.getScript('src/js/torrent.js', function() {
+          $.getScript('src/js/directory.js', function() {
+            $.getScript('src/js/list.js', function() {
+              $.getScript('src/js/mediainfo.js', function() {
+                $.getScript('src/js/event.js', function() {
+                  $.getScript('src/js/pnotif/pnotif.js', function() {
+                    $.getScript('src/js/popup/popup.js', function() {
+                      socket.emit('ready')
+                    })
                   })
                 })
               })
