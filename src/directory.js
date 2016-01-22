42ab3107c5a465dd47569f1853b3c140bf02eda2
Lunik <guillaume.lunik@gmail.com>
2016-01-22 21:05:10 +0100

    unSync some directory function

--- src/directory.js
+++ src/directory.js
@@ -37,28 +37,41 @@ Directory.prototype.list = function (dir) {
 
 Directory.prototype.remove = function (file) {
   Log.print('Remove file: ' + file)
-  var stats = fs.statSync(this.path + file)
-  if (stats.isDirectory()) {
-    removeRecursif(this.path + file)
-  } else {
-    fs.unlinkSync(this.path + file)
-  }
+  fs.stat(this.path + file, function(err, stats){
+    if(err) Log.print(err)
+    if (stats.isDirectory()) {
+      removeRecursif(instDirectory.path + file)
+    } else {
+      fs.unlink(instDirectory.path + file, function(err){
+        if(err) Log.print(err)
+      })
+    }
+  })
+
 }
 
 Directory.prototype.rename = function (path, oldname, newname) {
   Log.print('Rename: ' + oldname + ' In: ' + newname)
-  fs.renameSync(this.path + path + '/' + oldname, this.path + path + '/' + newname)
+  fs.rename(this.path + path + '/' + oldname, this.path + path + '/' + newname, function(err){
+    if(err) Log.print(err)
+  })
 }
 
 Directory.prototype.mkdir = function (path, name) {
   Log.print('Mkdir: ' + path + '/' + name)
-  fs.mkdirSync(this.path + path + '/' + name)
+  fs.mkdir(this.path + path + '/' + name, function(err){
+    if(err) Log.print(err)
+  })
 }
 
 Directory.prototype.mv = function (path, file, folder) {
   Log.print('Move: ' + file + ' into ' + path + folder)
-  fs.renameSync(this.path + path + file, this.path + path + folder + '/' + file)
+  fs.rename(this.path + path + file, this.path + path + folder + '/' + file, function(err){
+    if(err) Log.print(err)
+  })
 }
+
+
 function removeRecursif (path) {
   if (fs.existsSync(path)) {
     fs.readdirSync(path).forEach(function (file, index) {
