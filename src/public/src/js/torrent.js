e8dcf9dbf3850ac2ebde962f344158fa54ee42a4
Lunik <guillaume.lunik@gmail.com>
2016-01-24 12:43:27 +0100

    fix torrent url finder

--- src/public/src/js/torrent.js
+++ src/public/src/js/torrent.js
@@ -6,7 +6,7 @@ $searchResultTable = $('.menu .search-result')
 
 $torrentInput.keyup(function () {
   var value = $(this).val()
-  if (value.search('.torrent') != -1 || value.search('http://')) {
+  if (value.search('.torrent') != -1 || value.search('http://') != -1) {
     $startTorrentBut.show()
     $searchTorrentBut.hide()
     $currentSubmit = $startTorrentBut
