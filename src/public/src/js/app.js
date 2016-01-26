ac5d38e1da6d31f0d1646602d751475662f916c5
Lunik <guillaume.lunik@gmail.com>
2016-01-26 21:15:55 +0100

    remove socket io

--- src/public/src/js/app.js
+++ src/public/src/js/app.js
@@ -1,27 +1,23 @@
 $(document).ready(init)
 
-//var socket = io()
+// var socket = io()
 
 function init () {
   $.getScript('src/js/storage.js', function () {
-    //$.getScript('src/js/socket.js', function () {
-      $.getScript('src/js/file.js', function () {
-        $.getScript('src/js/torrent.js', function () {
-          $.getScript('src/js/directory.js', function () {
-            $.getScript('src/js/list.js', function () {
-              $.getScript('src/js/mediainfo.js', function () {
-                $.getScript('src/js/event.js', function () {
-                  $.getScript('src/js/pnotif/pnotif.js', function () {
-                    $.getScript('src/js/popup/popup.js', function () {
-                      //socket.emit('ready')
-                    })
-                  })
+    $.getScript('src/js/file.js', function () {
+      $.getScript('src/js/torrent.js', function () {
+        $.getScript('src/js/directory.js', function () {
+          $.getScript('src/js/list.js', function () {
+            $.getScript('src/js/mediainfo.js', function () {
+              $.getScript('src/js/event.js', function () {
+                $.getScript('src/js/pnotif/pnotif.js', function () {
+                  $.getScript('src/js/popup/popup.js', function () {})
                 })
               })
             })
           })
         })
       })
-    //})
+    })
   })
 }
