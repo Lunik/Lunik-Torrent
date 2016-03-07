eb74aa295542ae3c69ab062f44bee3ca3c595c94
Lunik <guillaume.lunik@gmail.com>
2016-03-07 08:41:54 +0100

    fix again

--- src/public/src/js/app/directory.js
+++ src/public/src/js/app/directory.js
@@ -18,7 +18,7 @@ function _Directory () {
   this.refresh = 30000
 
   $(window).bind('hashchange', function () {
-    this.table.html(' ')
+    this.body.html('')
     self.getList()
     self.setActions('', {
       download: false,
