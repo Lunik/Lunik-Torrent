03a18b2fe5fe2c6b05423d7eeca1849428a10d4f
Lunik <guillaume.lunik@gmail.com>
2016-03-14 09:10:43 +0100

    change the way to load module with requirejs

--- src/public/src/js/app/app.js
+++ src/public/src/js/app/app.js
@@ -1,18 +1,8 @@
-$.getScript('src/js/app/format.js')
-$.getScript('src/js/pnotif/pnotif.js')
-$.getScript('src/js/storage.js')
-$.getScript('src/js/app/mediainfo.js')
-$.getScript('src/js/jquery/jquery-ui.min.js')
-$.getScript('src/js/jquery/jquery-ui.touch-punch.min.js')
-$.getScript('src/js/app/directory.js')
-$.getScript('src/js/app/torrent.js')
-$.getScript('src/js/app/top-menu.js')
-$.getScript('src/js/jquery/jquery.tablesorter.min.js')
-$.getScript('src/js/app/list.js')
-$.getScript('src/js/app/searchtorrent.js')
-$.getScript('src/js/pnotif/pnotif.js')
-$.getScript('src/js/app/mediainfo.js')
-$.getScript('src/js/app/searchtorrent.js')
-$.getScript('src/js/storage.js')
-$.getScript('src/js/popup/popup.js')
-$.getScript('src/js/app/left-menu.js')
+requirejs(['top-menu', 'format', '../pnotif/pnotif', '../storage', '../jquery/jquery-ui.min', '../jquery/jquery-ui.touch-punch.min', '../popup/popup', '../jquery/jquery.tablesorter.min'], function () {
+  requirejs(['directory', 'torrent', 'mediainfo', 'searchtorrent'], function () {
+    requirejs(['list'], function(){
+      requirejs(['left-menu'])
+    })
+
+  })
+})
