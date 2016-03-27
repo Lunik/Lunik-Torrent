bbbfa6842fa3741265d0927a9b976a8a432e4ee3
Lunik <guillaume.lunik@gmail.com>
2016-03-27 12:22:16 +0200

    fix jquery ui drag and drop on mobile

--- src/public/src/js/app/app.js
+++ src/public/src/js/app/app.js
@@ -1,8 +1,10 @@
-requirejs(['top-menu', 'format', '../pnotif/pnotif', '../storage', '../jquery/jquery-ui.min', '../jquery/jquery-ui.touch-punch.min', '../popup/popup', '../jquery/jquery.tablesorter.min'], function () {
-  requirejs(['directory', 'torrent', 'mediainfo', 'searchtorrent'], function () {
+requirejs(['../jquery/jquery.min'], function(){
+requirejs(['top-menu', 'format', '../pnotif/pnotif', '../storage', '../jquery/jquery-ui.min', '../popup/popup', '../jquery/jquery.tablesorter.min'], function () {
+  requirejs(['../jquery/jquery-ui.touch-punch.min', 'directory', 'torrent', 'mediainfo', 'searchtorrent'], function () {
     requirejs(['list'], function () {
       requirejs(['left-menu'])
     })
 
   })
+})
 })
