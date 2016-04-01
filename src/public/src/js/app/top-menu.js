a89f5a6f92de81f3d88f0db9d296d39e04432362
Lunik <guillaume.lunik@gmail.com>
2016-04-01 19:05:35 +0200

    change config variable name

--- src/public/src/js/app/top-menu.js
+++ src/public/src/js/app/top-menu.js
@@ -1,7 +1,7 @@
 function _TopMenu () {
   var self = this
   this.ariane = $('.top-menu .ariane')
-  this.config = new Config()
+  this.Config = new Config()
 
   $(window).bind('hashchange', function () {
     self.setAriane()
