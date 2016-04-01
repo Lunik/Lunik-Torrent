b56781032363dda4650887924a6d48069453410b
Lunik <guillaume.lunik@gmail.com>
2016-04-01 17:15:19 +0200

    prepare config menu

--- src/public/src/js/app/app.js
+++ src/public/src/js/app/app.js
@@ -1,10 +1,12 @@
-requirejs(['../jquery/jquery.min'], function(){
-requirejs(['top-menu', 'format', '../pnotif/pnotif', '../storage', '../jquery/jquery-ui.min', '../popup/popup', '../jquery/jquery.tablesorter.min'], function () {
-  requirejs(['../jquery/jquery-ui.touch-punch.min', 'directory', 'torrent', 'mediainfo', 'searchtorrent'], function () {
-    requirejs(['list'], function () {
-      requirejs(['left-menu'])
-    })
+requirejs(['../jquery/jquery.min'], function () {
+  requirejs(['top-menu', 'format', '../pnotif/pnotif', '../storage', '../jquery/jquery-ui.min', '../popup/popup', '../jquery/jquery.tablesorter.min'], function () {
+    requirejs(['../jquery/jquery-ui.touch-punch.min', 'directory', 'torrent', 'mediainfo', 'searchtorrent'], function () {
+      requirejs(['list'], function () {
+        requirejs(['left-menu'], function () {
+          requirejs(['config'])
+        })
+      })
 
+    })
   })
-})
 })
