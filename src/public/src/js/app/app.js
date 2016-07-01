a15b45b0d6f1249b807bca44d0142675cb40daf0
gmartine <guillaume.martinez@cc.in2p3.fr>
2016-07-01 16:02:43 +0200

    use bower modules

--- src/public/src/js/app/app.js
+++ src/public/src/js/app/app.js
@@ -1,6 +1,6 @@
-requirejs(['../jquery/jquery.min'], function () {
-  requirejs(['format', '../pnotif/pnotif', '../storage', '../jquery/jquery-ui.min', '../popup/popup', '../jquery/jquery.tablesorter.min'], function () {
-    requirejs(['../jquery/jquery-ui.touch-punch.min', 'config', 'directory', 'torrent', 'mediainfo', 'searchtorrent'], function () {
+requirejs(['/src/bower_components/jquery/dist/jquery.min.js'], function () {
+  requirejs(['format', '../pnotif/pnotif', '../storage', '/src/bower_components/jquery-ui/jquery-ui.min.js', '../popup/popup', '/src/bower_components/tablesorter/jquery.tablesorter.min.js'], function () {
+    requirejs(['/src/bower_components/jquery-ui-touch-punch-improved/jquery.ui.touch-punch-improved.js', 'config', 'directory', 'torrent', 'mediainfo', 'searchtorrent'], function () {
       requirejs(['list', 'top-menu'], function () {
         requirejs(['left-menu'], function () {})
       })
