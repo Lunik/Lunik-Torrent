99b0383cdcd34759a0fbadc962ecfc6058a1823c
gmartine <guillaume.martinez@cc.in2p3.fr>
2016-07-07 10:03:18 +0200

    change import path

--- src/public/src/js/app/app.js
+++ src/public/src/js/app/app.js
@@ -1,6 +1,6 @@
-requirejs(['/src/bower_components/jquery/dist/jquery.min.js'], function () {
-  requirejs(['format', 'src/bower_components/notify.me/dist/js/notify-me.js', '../storage', '/src/bower_components/jquery-ui/jquery-ui.min.js', '../popup/popup', '/src/bower_components/tablesorter/jquery.tablesorter.min.js'], function () {
-    requirejs(['/src/bower_components/jquery-ui-touch-punch-improved/jquery.ui.touch-punch-improved.js', 'config', 'directory', 'torrent', 'mediainfo', 'searchtorrent'], function () {
+requirejs(['../../bower_components/jquery/dist/jquery.min'], function () {
+  requirejs(['format', '../../bower_components/notify.me/dist/js/notify-me', '../storage', '../../bower_components/jquery-ui/jquery-ui.min', '../popup/popup', '/src/bower_components/tablesorter/jquery.tablesorter.min.js'], function () {
+    requirejs(['../../bower_components/jquery-ui-touch-punch-improved/jquery.ui.touch-punch-improved', 'config', 'directory', 'torrent', 'mediainfo', 'searchtorrent'], function () {
       requirejs(['list', 'top-menu'], function () {
         requirejs(['left-menu'], function () {})
       })
