14e0e8c2fd0e86eba0c69ea34b2d854a9666d400
gmartine <guillaume.martinez@cc.in2p3.fr>
2016-07-08 10:50:09 +0200

    more client doc

--- src/client.js
+++ src/client.js
@@ -29,16 +29,20 @@ Client.prototype.download = function (torrentLink) {
     self.torrentLink = torrentLink
     Log.echo('Start: ' + torrentLink)
 
+    //download the torrent
     self.client.add(torrentLink, {
       path: config.torrent.downloads
     }, function (torrent) {
+      // On torrent start
       self.torrent = torrent
       Log.print('Start torrent: ' + self.torrent.name)
+      //emit start function with infoHash
       self.startFunction(torrent.infoHash)
 
       self.torrent.on('download', function (chunkSize) {
         var currentTime = new Date().getTime()
         if ((currentTime - self.timeout) > config.client.timeout) {
+          //emit update function with torrent infos
           self.updateFunction(self.getTorrent())
           self.timeout = currentTime
         }
@@ -46,11 +50,13 @@ Client.prototype.download = function (torrentLink) {
 
       self.torrent.on('done', function () {
         Log.print('Finish torrent: ' + self.torrent.name)
+        //emit done function with torrent hash and name
         self.doneFunction(self.torrent.infoHash, self.torrent.name)
       })
 
       self.torrent.on('noPeers', function () {
         Log.print('No peers: ' + self.torrent.name)
+        //emit done function with torrent hash and name
         self.doneFunction(self.torrent.infoHash, self.torrent.name)
       })
 
