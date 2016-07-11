3aea106708519f009924ccffd65dee50b22e2a71
Lunik <guillaume.lunik@gmail.com>
2016-07-11 22:46:13 +0200

    add local-storage-api

--- src/public/src/js/app.js
+++ src/public/src/js/app.js
@@ -2,6 +2,7 @@ var App = {}
 
 requirejs.config({
   paths: {
+    'localstorage': '../bower_components/local-storage-api/dist/storage.min',
     'jquery': '../bower_components/jquery/dist/jquery.min',
     'jquery-ui': '../bower_components/jquery-ui/jquery-ui.min',
     'vue': '../bower_components/vue/dist/vue.min',
@@ -13,11 +14,14 @@ requirejs.config({
 })
 
 requirejs([
+  'localstorage',
   'jquery',
   'jquery-ui',
   'vue'
-], function (jq, ui, v) {
+], function (ls, jq, ui, v) {
+  console.log(ls)
   App.Vue = v
+  App.ls = ls
   requirejs([
     'jquery.ui.touch-punch',
     'tablesorter',
