471cff163a05fdfd06ad06c37325fe69ae356a2f
Lunik <guillaume.lunik@gmail.com>
2016-07-12 19:15:09 +0200

    app as a class

--- src/public/src/js/app.js
+++ src/public/src/js/app.js
@@ -1,37 +1,44 @@
-//Variable global avec tous les modules
-var App = {}
+function App () {
+  var self = this
+  // Configure le path pour les bower_components
+  requirejs.config({
+    paths: {
+      'localstorage': '../bower_components/local-storage-api/dist/storage.min',
+      'jquery': '../bower_components/jquery/dist/jquery.min',
+      'jquery-ui': '../bower_components/jquery-ui/jquery-ui.min',
+      'vue': '../bower_components/vue/dist/vue.min',
+      'jquery.ui.touch-punch': '../bower_components/jquery-ui-touch-punch-improved/jquery.ui.touch-punch-improved',
+      'tablesorter': '../bower_components/tablesorter/jquery.tablesorter.min',
+      'notify-me': '../bower_components/notify.me/dist/js/notify-me',
+      'popup': '../bower_components/popupjs/dist/popup.min'
+    }
+  })
 
-// Configure le path pour les bower_components
-requirejs.config({
-  paths: {
-    'localstorage': '../bower_components/local-storage-api/dist/storage.min',
-    'jquery': '../bower_components/jquery/dist/jquery.min',
-    'jquery-ui': '../bower_components/jquery-ui/jquery-ui.min',
-    'vue': '../bower_components/vue/dist/vue.min',
-    'jquery.ui.touch-punch': '../bower_components/jquery-ui-touch-punch-improved/jquery.ui.touch-punch-improved',
-    'tablesorter': '../bower_components/tablesorter/jquery.tablesorter.min',
-    'notify-me': '../bower_components/notify.me/dist/js/notify-me',
-    'popup': '../bower_components/popupjs/dist/popup.min'
-  }
-})
-
-// Charge les modules
-requirejs([
-  'localstorage',
-  'jquery',
-  'jquery-ui',
-  'vue'
-], function (ls, jq, ui, v) {
-  App.Vue = v
-  App.ls = new Storage()
+  // Charge les modules
   requirejs([
-    'jquery.ui.touch-punch',
-    'tablesorter',
-    'notify-me',
-    'popup'
-  ], function () {
-    requirejs(['loading', 'top-menu'], function () {
-      App.Loading.hide()
+    'localstorage',
+    'jquery',
+    'jquery-ui',
+    'vue'
+  ], function (ls, jq, ui, v) {
+    self.Vue = v
+    self.Storage = new Storage()
+    requirejs([
+      'jquery.ui.touch-punch',
+      'tablesorter',
+      'notify-me',
+      'popup'
+    ], function () {
+      requirejs(['loading', 'top-menu'], function () {
+        self.Loading.hide()
+
+        $(window).bind('hashchange', function () {
+          self.hash = document.location.hash.substring(1)
+        })
+      })
     })
   })
-})
+}
+
+// Variable global avec tous les modules
+var App = new App()
