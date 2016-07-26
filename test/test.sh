79102a9116662de39f3faf2732dfd81bf1e47b01
Lunik <guillaume.lunik@gmail.com>
2016-07-26 19:49:57 +0200

    add ()

--- test/test.sh
+++ test/test.sh
@@ -5,16 +5,16 @@ DIRNAME2="ok2"
 FILENAME="thefile"
 MAGNET="magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4" HASH="6a9759bffd5c0af65319979fb7832189f4f3c35d"
 
-function start {
+function start () {
   npm start &
   pid=$!
 }
 
-function stop {
+function stop () {
   kill $pid
 }
 
-function checkUp {
+function checkUp () {
   curl -s http://localhost:$PORT >/dev/null
   code=$?
   if [[ $code -ne 0 ]]
