ebb11194a72eb83c385bdf68a9a7d50d75fb0bab
Lunik <guillaume.lunik@gmail.com>
2016-08-09 20:48:56 +0200

    add form view and loading

--- src/public/src/js/auth.js
+++ src/public/src/js/auth.js
@@ -1,17 +1,39 @@
+var App = {}
 ;(function () {
-  requirejs.config({
-    paths: {
-      'jquery': '../bower_components/jquery/dist/jquery.min',
-      'crypto-js': '../bower_components/crypto-js/crypto-js',
-      'vue': '../bower_components/vue/dist/vue.min',
-      'notify-me': '../bower_components/notify.me/dist/js/notify-me'
-    }
-  })
-
-  // load modules
-  requirejs(['jquery', 'crypto-js', 'vue'], function (jq, crypto, vue) {
-    requirejs(['notify-me'], function(notif){
-
+  function _App () {
+    var self = this
+    requirejs.config({
+      paths: {
+        'jquery': '../bower_components/jquery/dist/jquery.min',
+        'crypto-js': '../bower_components/crypto-js/crypto-js',
+        'vue': '../bower_components/vue/dist/vue.min',
+        'notify-me': '../bower_components/notify.me/dist/js/notify-me'
+      }
     })
-  })
+
+    // load modules
+    requirejs(['jquery', 'crypto-js', 'vue'], function (jq, crypto, vue) {
+      self.Vue = vue
+      self.v = new self.Vue({
+        el: '.form',
+        data: {
+          login: {
+            user: '',
+            pass: ''
+          },
+          register: {
+            user: '',
+            pass: '',
+            pass2: ''
+          }
+        }
+      })
+      requirejs(['notify-me', 'format', 'loading'], function(notif){
+        App.Loading.hide('app')
+      })
+    })
+  }
+
+
+  App = new _App()
 })()
