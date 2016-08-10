e591837912a56a5c88b455da7978876e75248376
Lunik <guillaume.lunik@gmail.com>
2016-08-10 19:19:07 +0200

    use relative path

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
+parent_path="$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
 
 if ! [ -f configs/passwords.json ]
-  then echo "==> Creating htpasswd"
-  echo '{}' > configs/passwords.json
+  then echo "==> Creating passwords.json"
+  echo '{}' > $parent_path/../configs/passwords.json
 fi
 
 if ! [ -f configs/fileInfo.json ]
   then echo "==> Creating fileInfo"
-  echo '{}' >> configs/fileInfo.json
+  echo '{}' >> $parent_path/../configs/fileInfo.json
 fi
 
 for dir in files downloads logs;
 do
   if ! [ -d $dir ]
     then echo "==> Creating $dir"
-    mkdir $dir
+    mkdir $parent_path/../$dir
   fi
 done
 
 if ! [ -f torrents.txt ]
   then echo "==> Creating torrents"
-  touch torrents.txt
+  touch $parent_path/../torrents.txt
 fi
 
 if ! [ -f configs/config.json ]
   then echo "==> Copying configs"
-  cp configs/config.default configs/config.json
+  cp $parent_path/../configs/config.default $parent_path/../configs/config.json
 fi
