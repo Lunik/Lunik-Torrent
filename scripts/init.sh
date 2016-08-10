63af6657dcb74c39f25fa7aac19fc2f66327a780
Lunik <guillaume.lunik@gmail.com>
2016-08-10 21:08:45 +0200

    fix scrips relative path

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,28 +3,28 @@ parent_path="$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
 
 if ! [ -f configs/passwords.json ]
   then echo "==> Creating passwords.json"
-  echo '{}' > $parent_path/../configs/passwords.json
+  echo '{}' > $parent_path/configs/passwords.json
 fi
 
 if ! [ -f configs/fileInfo.json ]
   then echo "==> Creating fileInfo"
-  echo '{}' >> $parent_path/../configs/fileInfo.json
+  echo '{}' >> $parent_path/configs/fileInfo.json
 fi
 
 for dir in files downloads logs;
 do
   if ! [ -d $dir ]
     then echo "==> Creating $dir"
-    mkdir $parent_path/../$dir
+    mkdir $parent_path/$dir
   fi
 done
 
 if ! [ -f torrents.txt ]
   then echo "==> Creating torrents"
-  touch $parent_path/../torrents.txt
+  touch $parent_path/torrents.txt
 fi
 
 if ! [ -f configs/config.json ]
   then echo "==> Copying configs"
-  cp $parent_path/../configs/config.default $parent_path/../configs/config.json
+  cp $parent_path/configs/config.default $parent_path/configs/config.json
 fi
