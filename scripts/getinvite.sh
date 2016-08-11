a8447cf3bc6c50e104eabff85a771a9e8712382d
gmartine <guillaume.martinez@cc.in2p3.fr>
2016-08-11 09:17:01 +0200

    remove relative path

--- scripts/getinvite.sh
+++ scripts/getinvite.sh
@@ -2,8 +2,8 @@
 
 parent_path="$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
 
-key=$(cat "$parent_path/configs/config.json" | grep "invitationKey" | sed -e 's/^.*: *\"\([^\"]*\)\"/\1/')
-port=$(cat "$parent_path/configs/config.json" | grep "port" | sed -e 's/^.*: *\([0-9]*\)\,/\1/')
+key=$(cat "configs/config.json" | grep "invitationKey" | sed -e 's/^.*: *\"\([^\"]*\)\"/\1/')
+port=$(cat "configs/config.json" | grep "port" | sed -e 's/^.*: *\([0-9]*\)\,/\1/')
 invite=$(curl -s --data "invitationkey=$key" http://localhost:$port/auth?todo=invite | sed -e 's/^.*: *\"\([^\"]*\)\"}/\1/')
 
 echo $invite
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,30 +1,29 @@
 #!/bin/sh
-parent_path="$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
 
 if ! [ -f configs/passwords.json ]
   then echo "==> Creating passwords.json"
-  echo '{}' > $parent_path/configs/passwords.json
+  echo '{}' > configs/passwords.json
 fi
 
 if ! [ -f configs/fileInfo.json ]
   then echo "==> Creating fileInfo"
-  echo '{}' >> $parent_path/configs/fileInfo.json
+  echo '{}' >> configs/fileInfo.json
 fi
 
 for dir in files downloads logs;
 do
   if ! [ -d $dir ]
     then echo "==> Creating $dir"
-    mkdir $parent_path/$dir
+    mkdir $dir
   fi
 done
 
 if ! [ -f torrents.txt ]
   then echo "==> Creating torrents"
-  touch $parent_path/torrents.txt
+  touch torrents.txt
 fi
 
 if ! [ -f configs/config.json ]
   then echo "==> Copying configs"
-  cp $parent_path/configs/config.default $parent_path/configs/config.json
+  cp configs/config.default configs/config.json
 fi
