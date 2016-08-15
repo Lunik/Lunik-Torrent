62f606ca2349d00bdf8b7102ea45b1ccc28afc0c
Lunik <guillaume.lunik@gmail.com>
2016-08-15 10:05:38 +0200

    add config worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 // launch the server
-var Server = require('./server.js')
 
-var update = require('./update.js')
+var path = require('path')
+
+global.__base = path.join(__dirname, '..', '/')
+
+var Server = require(path.join(__base, 'src/server.js'))
+var Update = require(path.join(__base, 'src/update.js'))
+var Config = require(path.join(__base, 'src/config.js'))
+
+var ConfigWorker = new Config()
+global.__config = ConfigWorker.load(path.join(__base, 'configs/config.json'))
 
 // this function is called when you want the server to die gracefully
 // i.e. wait for existing connections
