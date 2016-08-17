b6b88a0b2a40bf9c25b8bcfca27c32e2d36b2d6a
Lunik <guillaume.lunik@gmail.com>
2016-08-17 21:10:43 +0200

    standard

--- test/test.js
+++ test/test.js
@@ -17,109 +17,109 @@ var assert = require('chai').assert
 describe('Fontend', function () {})
 
 describe('Backend', function () {
-  describe('Auth', function(){
+  describe('Auth', function () {
     var username = 'foo' + rand.rand()
     var username2 = 'foo2' + rand.rand()
 
     var Auth = require(path.join(__base, 'src/auth.js'))
-    describe('createInvite()', function(){
-      it('Invite key: ' + __config.server.invitationKey, function(done){
+    describe('createInvite()', function () {
+      it('Invite key: ' + __config.server.invitationKey, function (done) {
         assert.typeOf(Auth.createInvite(__config.server.invitationKey), 'string')
         done()
       })
-      it('Invite key: Unknown', function(done){
+      it('Invite key: Unknown', function (done) {
         assert(!Auth.createInvite(''))
         done()
       })
     })
-    describe('Register()', function(){
-      it('User: foo, Pass: bar, Invite: Valid invitation', function(done){
+    describe('Register()', function () {
+      it('User: foo, Pass: bar, Invite: Valid invitation', function (done) {
         var invite = Auth.createInvite(__config.server.invitationKey)
         assert.typeOf(Auth.register(username, 'bar', invite), 'string')
         done()
       })
-      it('User: foo, Pass: bar, Invite: Valid invitation', function(done){
+      it('User: foo, Pass: bar, Invite: Valid invitation', function (done) {
         var invite = Auth.createInvite(__config.server.invitationKey)
         assert(!Auth.register(username, 'bar', invite))
         done()
       })
-      it('User: foo, Pass: bar, Invite: Invalid invitation', function(done){
+      it('User: foo, Pass: bar, Invite: Invalid invitation', function (done) {
         assert(!Auth.register(username2, 'bar', ''))
         done()
       })
     })
-    describe('Loggin()', function(){
-      it('User: foo, Pass: bar', function(done){
+    describe('Loggin()', function () {
+      it('User: foo, Pass: bar', function (done) {
         assert.typeOf(Auth.login(username, 'bar'), 'string')
         done()
       })
-      it('User: Unknown, Pass: bar', function(done){
+      it('User: Unknown, Pass: bar', function (done) {
         assert(!Auth.login(username2, 'bar'))
         done()
       })
-      it('User: foo, Pass: Wrong', function(done){
+      it('User: foo, Pass: Wrong', function (done) {
         assert(!Auth.login(username, 'test'))
         done()
       })
     })
-    describe('Logout()', function(){
-      it('User: foo, Pass: bar', function(done){
+    describe('Logout()', function () {
+      it('User: foo, Pass: bar', function (done) {
         var token = Auth.login(username, 'bar')
         assert(Auth.logout(username, token))
         done()
       })
-      it('User: Unknown, Pass: bar', function(done){
+      it('User: Unknown, Pass: bar', function (done) {
         assert(!Auth.logout(username2, ''))
         done()
       })
-      it('User: foo, Pass: Wrong', function(done){
+      it('User: foo, Pass: Wrong', function (done) {
         var token = Auth.login(username, 'bar')
-        assert(!Auth.logout(username, token+'1'))
+        assert(!Auth.logout(username, token + '1'))
         done()
       })
     })
-    describe('CheckLogged()', function(){
-      it('User: foo', function(done){
+    describe('CheckLogged()', function () {
+      it('User: foo', function (done) {
         var token = Auth.login(username, 'bar')
         assert(Auth.checkLogged(username, token))
         done()
       })
-      it('User: Unknown', function(done){
+      it('User: Unknown', function (done) {
         assert(!Auth.checkLogged(username2, ''))
         done()
       })
     })
   })
-  describe('MediaInfo', function(){
+  describe('MediaInfo', function () {
     var MediaInfo = require(path.join(__base, 'src/mediaInfo.js'))
-    describe('GetInfo()', function(){
+    describe('GetInfo()', function () {
       this.timeout(30000)
-      it('Type: series, Query: Game of thrones', function(done){
-        MediaInfo.getInfo('series', 'Game of Thrones', function(res){
+      it('Type: series, Query: Game of thrones', function (done) {
+        MediaInfo.getInfo('series', 'Game of Thrones', function (res) {
           assert.equal(res.query, 'Game of Thrones')
           done()
         })
       })
-      it('Type: movie, Query: Alien', function(done){
-        MediaInfo.getInfo('films', 'Alien', function(res){
+      it('Type: movie, Query: Alien', function (done) {
+        MediaInfo.getInfo('films', 'Alien', function (res) {
           assert.equal(res.query, 'Alien')
           done()
         })
       })
-      it('Type: movie, Query: Unknown', function(done){
-        MediaInfo.getInfo('films', 'blbablabla', function(res){
+      it('Type: movie, Query: Unknown', function (done) {
+        MediaInfo.getInfo('films', 'blbablabla', function (res) {
           assert.typeOf(res.err, 'string')
           done()
         })
       })
     })
   })
-  describe('SearchT', function(){
+  describe('SearchT', function () {
     var SearchT = require(path.join(__base, 'src/searchT.js'))
-    describe('Search()', function(){
+    describe('Search()', function () {
       this.timeout(30000)
-      it('Search: Game of thrones', function(done){
-        SearchT.search('Game of Thrones', function(res){
+      it('Search: Game of thrones', function (done) {
+        SearchT.search('Game of Thrones', function (res) {
           assert.typeOf(res, 'object')
           assert(res.tven)
           assert(res.tvfr)
@@ -128,10 +128,10 @@ describe('Backend', function () {
         })
       })
     })
-    describe('Latest()', function(){
+    describe('Latest()', function () {
       this.timeout(30000)
-      it('Get latest', function(done){
-        SearchT.latest(function(res){
+      it('Get latest', function (done) {
+        SearchT.latest(function (res) {
           assert.typeOf(res, 'object')
           assert(res.tv)
           assert(res.mv)
@@ -140,25 +140,25 @@ describe('Backend', function () {
       })
     })
   })
-  describe('Client', function(){
+  describe('Client', function () {
     var Client = require(path.join(__base, 'src/client.js'))
     var ClientWorker = new Client()
-    describe('On()', function(){
-      it('Add startFunction', function(done){
-        ClientWorker.on('start', function(hash){
+    describe('On()', function () {
+      it('Add startFunction', function (done) {
+        ClientWorker.on('start', function (hash) {
           assert.typeOf(hash, 'string')
         })
         done()
       })
-      it('Add updateFunction', function(done){
-        ClientWorker.on('download', function(torrent){
+      it('Add updateFunction', function (done) {
+        ClientWorker.on('download', function (torrent) {
           assert.typeOf(torrent, 'object')
           assert.typeOf(torrent.name, 'string')
         })
         done()
       })
-      it('Add doneFunction', function(done){
-        ClientWorker.on('done', function(err, hash, name){
+      it('Add doneFunction', function (done) {
+        ClientWorker.on('done', function (err, hash, name) {
           assert(!err)
           assert.typeOf(hash, 'string')
           assert.typeOf(name, 'string')
@@ -166,10 +166,10 @@ describe('Backend', function () {
         done()
       })
     })
-    describe('Dowload()', function(){
+    describe('Dowload()', function () {
       this.timeout(300000)
-      it('Dowload sintel', function(done){
-        ClientWorker.on('done', function(err, hash, name){
+      it('Dowload sintel', function (done) {
+        ClientWorker.on('done', function (err, hash, name) {
           ClientWorker.stop()
           done()
         })
@@ -177,64 +177,64 @@ describe('Backend', function () {
       })
     })
   })
-  describe('Directory', function(){
+  describe('Directory', function () {
     var Directory = require(path.join(__base, 'src/directory.js'))
-    describe('saveFileInfo()', function(){
-      it('Save', function(done){
+    describe('saveFileInfo()', function () {
+      it('Save', function (done) {
         Directory.saveFileInfo()
         done()
       })
     })
-    describe('Mkdir()', function(){
-      it('Create dir', function(done){
-        Directory.mkdir('/', 'ok'+rand.rand())
+    describe('Mkdir()', function () {
+      it('Create dir', function (done) {
+        Directory.mkdir('/', 'ok' + rand.rand())
         done()
       })
     })
-    describe('List()', function(){
-      it('Scan / ', function(done){
+    describe('List()', function () {
+      it('Scan / ', function (done) {
         assert.typeOf(Directory.list('/'), 'object')
         done()
       })
     })
-    describe('Mv()', function(){
-      it('Mv dir into dir', function(done){
-        var recip = 'ok'+rand.rand()
-        var dir = 'ok'+rand.rand()
+    describe('Mv()', function () {
+      it('Mv dir into dir', function (done) {
+        var recip = 'ok' + rand.rand()
+        var dir = 'ok' + rand.rand()
         Directory.mkdir('/', recip)
         Directory.mkdir('/', dir)
         Directory.mv('/', dir, recip)
         done()
       })
     })
-    describe('Rename()', function(){
-      it('change dir name', function(done){
-        var dir = 'ok'+rand.rand()
-        var newname = 'ok'+rand.rand()
+    describe('Rename()', function () {
+      it('change dir name', function (done) {
+        var dir = 'ok' + rand.rand()
+        var newname = 'ok' + rand.rand()
         Directory.mkdir('/', dir)
         Directory.rename('/', dir, newname)
         done()
       })
     })
-    describe('SetOwner()', function(){
-      it('setOwner of dir', function(done){
-        var dir = 'ok'+rand.rand()
+    describe('SetOwner()', function () {
+      it('setOwner of dir', function (done) {
+        var dir = 'ok' + rand.rand()
         Directory.mkdir('/', dir)
         Directory.setOwner(dir, 'test')
         done()
       })
     })
-    describe('Remove()', function(){
-      it('remove dir', function(done){
-        var dir = 'ok'+rand.rand()
+    describe('Remove()', function () {
+      it('remove dir', function (done) {
+        var dir = 'ok' + rand.rand()
         Directory.mkdir('/', dir)
         Directory.remove(dir)
         done()
       })
     })
-    describe('Downloading()', function(){
-      it('setDownloading', function(done){
-        var dir = 'ok'+rand.rand()
+    describe('Downloading()', function () {
+      it('setDownloading', function (done) {
+        var dir = 'ok' + rand.rand()
         Directory.mkdir('/', dir)
         Directory.setDownloading(dir)
         Directory.setDownloading(dir)
@@ -243,7 +243,7 @@ describe('Backend', function () {
         Directory.finishDownloading(dir)
         done()
       })
-      it('updateDownloads', function(done){
+      it('updateDownloads', function (done) {
         Directory.updateDownloads()
         done()
       })
@@ -256,29 +256,29 @@ describe('Backend', function () {
         this.timeout(300000)
         Torrent.setDownloader('admin', 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4')
         fs.writeFile(path.join(__base, __config.torrent.scanTorrent),
-        'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
-        + 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
-        + 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
-        + 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
-        + 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4', function (err) {
-          assert(!err)
-          Torrent.startPointTorrent(Torrent)
-          setTimeout(function(){
-            done()
-          }, 60000)
-        })
+          'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
+          + 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
+          + 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
+          + 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
+          + 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4', function (err) {
+            assert(!err)
+            Torrent.startPointTorrent(Torrent)
+            setTimeout(function () {
+              done()
+            }, 60000)
+          })
       })
     })
     describe('remove()', function () {
       it('start and remove', function (done) {
         this.timeout(300000)
-          Torrent.start('magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4')
-          setTimeout(function(){
-            assert.typeOf(Torrent.getInfo(), 'object')
-            Torrent.getUrlFromHash('6a9759bffd5c0af65319979fb7832189f4f3c35d')
-            Torrent.remove('6a9759bffd5c0af65319979fb7832189f4f3c35d')
-            done()
-          }, 10000)
+        Torrent.start('magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4')
+        setTimeout(function () {
+          assert.typeOf(Torrent.getInfo(), 'object')
+          Torrent.getUrlFromHash('6a9759bffd5c0af65319979fb7832189f4f3c35d')
+          Torrent.remove('6a9759bffd5c0af65319979fb7832189f4f3c35d')
+          done()
+        }, 10000)
       })
     })
   })
@@ -287,16 +287,13 @@ describe('Backend', function () {
     var url = 'http://localhost:' + __config.server.port + '/'
     var user = 'test' + rand.rand()
     describe('POST and GET Auth', function () {
-    	this.timeout(60000)
       it('get / without login', function (done) {
-      	setTimeout(function(){
-	        request(url, function (err, res, body) {
-	          if (!err && res.statusCode == 200) {
-	            assert(body.match('<title>Login - Lunik - Torrent</title>'))
-	          }
-	          done()
-	        })
-        }, 30000)
+        request(url, function (err, res, body) {
+          if (!err && res.statusCode == 200) {
+            assert(body.match('<title>Login - Lunik - Torrent</title>'))
+          }
+          done()
+        })
       })
       it('gentoken + register + login', function (done) {
         // gentoken
