633955b728e1a2f6a9b8e85778fcc2a0c8f44e22
gmartine <guillaume.martinez@cc.in2p3.fr>
2016-08-23 11:40:48 +0200

    fix some tests

--- test/test.js
+++ test/test.js
@@ -173,7 +173,7 @@ describe('Backend', function () {
           ClientWorker.stop()
           done()
         })
-        ClientWorker.download('magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4')
+        ClientWorker.download('magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4', function(){})
       })
     })
   })
@@ -194,7 +194,7 @@ describe('Backend', function () {
     describe('List()', function () {
       it('Scan / ', function (done) {
         Directory.list('/', function(folder){
-          assert.typeOf(object, 'object')
+          assert.typeOf(folder, 'object')
           done()
         })
       })
