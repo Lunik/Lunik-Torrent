f0a469b7d76c7edff850c67d1a6de179784b9ab8
gmartine <guillaume.martinez@cc.in2p3.fr>
2016-08-23 11:26:45 +0200

    remove gracefull shutdown

--- src/index.js
+++ src/index.js
@@ -11,23 +11,3 @@ global.__config = ConfigWorker.load(Path.join(__base, 'configs/config.json'))
 
 var Server = require(Path.join(__base, 'src/worker/server.js'))
 var Update = require(Path.join(__base, 'src/worker/update.js'))
-
-
-// this function is called when you want the server to die gracefully
-// i.e. wait for existing connections
-var gracefulShutdown = function () {
-  Server.server.close(function () {
-    process.exit()
-  })
-
-  // if after
-  setTimeout(function () {
-    process.exit()
-  }, 10 * 1000)
-}
-
-// listen for TERM signal .e.g. kill
-process.on('SIGTERM', gracefulShutdown)
-
-// listen for INT signal e.g. Ctrl-C
-process.on('SIGINT', gracefulShutdown)
