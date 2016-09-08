7204dd540580ebd1bc6ba6c381679381f8509882
Lunik <guillaume.lunik@gmail.com>
2016-09-08 11:50:21 +0200

    new update module with auto update

--- src/worker/config.js
+++ src/worker/config.js
@@ -10,6 +10,8 @@ Config.prototype.load = function (path) {
 
   assert(config, messageError('config'))
 
+  assert(config.autoUpdate === false || config.autoUpdate === true, messageError('autoUpdate'))
+
   assert(config.log, messageError('log'))
   assert(config.log.path, messageError('log.path'))
 
--- src/worker/update.js
+++ src/worker/update.js
@@ -1,17 +1,43 @@
 'use strict'
 
-var checkUpdate = require('check-update-github')
 var Path = require('path')
+var spawn = require('child_process').spawn
+var version = require('version')
+
+var Log = require(Path.join(__base, 'src/worker/log.js'))
+var LogWorker = new Log({
+  module: 'Update'
+})
 
 var pkg = require(Path.join(__base, 'package.json'))
 
-checkUpdate({
-  name: pkg.name,
-  currentVersion: pkg.version,
-  user: 'Lunik',
-  branch: 'master'
-}, function (err, latestVersion, defaultMessage) {
-  if (!err) {
-    console.log(defaultMessage)
-  }
-})
+function Update (cb) {
+  version.fetch('lunik-torrent', function (error, version) {
+    if (error) {
+      LogWorker.error(error)
+      exit(1)
+    } else {
+      if (version === pkg.version) {
+        LogWorker.info('You have the last version:', version)
+        cb()
+      } else {
+        LogWorker.warning("You don't have the last version:", version)
+        if (__config.autoUpdate) {
+          LogWorker.info('Auto updating the app.')
+          var pull = spawn('git', ['pull'])
+          pull.on('exit', (code) => {
+            var checkout = spawn('git', ['checkout', 'tags/' + version])
+            checkout.on('exit', (code) => {
+              LogWorker.info('Update to ' + version + 'succeed.')
+              cb()
+            })
+          })
+        } else {
+          LogWorker.warning('Auto update is disabled.')
+          cb()
+        }
+      }
+    }
+  })
+}
+module.exports = Update
