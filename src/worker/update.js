b19f7b2ddd8ac04159e8684dd0ea9bb5075c6d19
Lunik <guillaume.lunik@gmail.com>
2016-09-08 12:01:24 +0200

    show git logs and handle error

--- src/worker/update.js
+++ src/worker/update.js
@@ -21,13 +21,27 @@ function Update (cb) {
         LogWorker.info('You have the last version: ' + version)
         cb()
       } else {
-        LogWorker.warning('You don\'t have the last version: ' + version)
+        LogWorker.warning("You don't have the last version: " + version)
         if (__config.autoUpdate) {
           LogWorker.info('Auto updating the app.')
           var pull = spawn('git', ['pull'])
+          bat.stdout.on('data', (data) => {
+            LogWorker.info(data)
+          })
           pull.on('exit', (code) => {
+            if(code){
+              Log.error('Git pull fail with code: ' + code)
+              exit(1)
+            }
             var checkout = spawn('git', ['checkout', 'tags/' + version])
+            checkout.stdout.on('data', (data) => {
+              LogWorker.info(data)
+            })
             checkout.on('exit', (code) => {
+              if(code){
+                Log.error('Git pull fail with code: ' + code)
+                exit(1)
+              }
               LogWorker.info('Update to ' + version + 'succeed.')
               cb()
             })
