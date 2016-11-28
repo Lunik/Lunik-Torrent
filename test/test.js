11e378bbe0efa532011294bb0a11d632b5885c4f
Lunik <guillaume.lunik@gmail.com>
2016-11-28 12:35:56 +0100

    right token decalration

--- test/test.js
+++ test/test.js
@@ -16,11 +16,11 @@ var Rand = require('crypto-rand')
 var Crypto = require('crypto-js')
 var token = Crypto.SHA256(Rand.rand().toString()).toString()
 
+global.__DBtoken = token
+
 var Database = require(path.join(__base, 'src/database/server.js'))
 var DBPort = process.env.DB_PORT || __config.database.port
 var DB = new Database(DBPort, token)
-
-global.__DBtoken = token
 
 var assert = require('chai').assert
 
