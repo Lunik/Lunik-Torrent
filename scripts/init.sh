38822b6ffb61ee82ab22d8556a70291e1e0079e9
Lunik <guillaume.lunik@gmail.com>
2016-12-08 17:23:23 +0100

    get back config script

--- scripts/init.sh
+++ scripts/init.sh
@@ -12,3 +12,12 @@ if ! [ -f torrents.txt ]
   then echo "==> Creating torrents"
   touch torrents.txt
 fi
+
+if ! [ -f configs/config.json ]
+  then echo "==> Copying configs"
+  cp configs/config.default configs/config.json
+  if [ ${#INVITATION_KEY} -gt 0 ]
+  then
+    sed -i -e 's/MY_KEY/'$INVITATION_KEY'/' configs/config.json
+  fi
+fi
