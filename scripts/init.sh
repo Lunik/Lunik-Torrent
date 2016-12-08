dbfc09c912084a257fc753838d786e90762b6f55
Lunik <guillaume.lunik@gmail.com>
2016-12-08 17:12:39 +0100

    remove useless scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,36 +8,7 @@ do
   fi
 done
 
-if [ -f configs/passwords.json ] && ! [ -f data/passwords.json ]
-  then echo "==> Moving passwords.json"
-  mv configs/passwords.json data/passwords.json
-fi
-
-if ! [ -f data/passwords.json ]
-  then echo "==> Creating passwords.json"
-  echo '{}' > data/passwords.json
-fi
-
-if [ -f configs/fileInfo.json ] && ! [ -f data/fileInfo.json ]
-  then echo "==> Moving passwords.json"
-  mv configs/fileInfo.json data/fileInfo.json
-fi
-
-if ! [ -f data/fileInfo.json ]
-  then echo "==> Creating fileInfo"
-  echo '{}' >> data/fileInfo.json
-fi
-
 if ! [ -f torrents.txt ]
   then echo "==> Creating torrents"
   touch torrents.txt
-fi
-
-if ! [ -f configs/config.json ]
-  then echo "==> Copying configs"
-  cp configs/config.default configs/config.json
-  if [ ${#INVITATION_KEY} -gt 0 ]
-  then
-    sed -i -e 's/MY_KEY/'$INVITATION_KEY'/' configs/config.json
-  fi
 fi
