e33ae4dd051fa254cdcf9bfb5c75a19d4385aadd
Lunik <guillaume.lunik@gmail.com>
2016-12-10 18:23:20 +0100

    clusterinse app

--- src/index.js
+++ src/index.js
@@ -12,14 +12,34 @@ global.__config = ConfigWorker.load(Path.join(__base, 'configs/config.json'))
 var cluster = require('cluster')
 var numCPUs = require('os').cpus().length
 
-var Rand = require('crypto-rand')
-var Crypto = require('crypto-js')
-var token = Crypto.SHA256(Rand.rand().toString()).toString()
-global.__DBtoken = token
+if (cluster.isMaster) {
+  var Rand = require('crypto-rand')
+  var Crypto = require('crypto-js')
+  var token = Crypto.SHA256(Rand.rand().toString()).toString()
 
-var Database = require(Path.join(__base, 'src/database/server.js'))
-var DBPort = process.env.DB_PORT || __config.database.port
-var DB = new Database(DBPort, token)
+  var Database = require(Path.join(__base, 'src/database/server.js'))
+  var DBPort = process.env.DB_PORT || __config.database.port
+  var DB = new Database(DBPort, token)
 
-var Server = require(Path.join(__base, 'src/controller/main.js'))
-var ServerWorker = new Server()
+  var i = 0
+
+  while (i < numCPUs) {
+    cluster.fork()
+    i++
+  }
+
+  cluster.on('online', function (worker) {
+    worker.send(token)
+  })
+
+  cluster.on('exit', function (worker, code, signal) {
+    console.log('Worker ' + worker.process.pid + ' died')
+    cluster.fork()
+  })
+} else {
+  var Server = require(Path.join(__base, 'src/controller/main.js'))
+  process.on('message', function (token) {
+    global.__DBtoken = token
+    var ServerWorker = new Server(cluster.worker.id)
+  })
+}
