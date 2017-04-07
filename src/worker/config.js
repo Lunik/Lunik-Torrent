7d141005b577e07cb17925dfd6db967d43cde14e
Lunik <guillaume.lunik@gmail.com>
2017-04-07 12:40:39 +0200

    test no authentification (#253)

--- src/worker/config.js
+++ src/worker/config.js
@@ -13,10 +13,6 @@ Config.prototype.load = function (path) {
   assert(config.log, messageError('log'))
   assert(config.log.path, messageError('log.path'))
 
-  assert(config.database, messageError('database'))
-  assert(config.database.port, messageError('database.port'))
-  assert(config.database.host, messageError('database.host'))
-
   assert(config.server, messageError('server'))
   assert(config.server.port, messageError('server.port'))
   assert(config.server.masterKey, messageError('server.masterKey'))
@@ -28,6 +24,13 @@ Config.prototype.load = function (path) {
     assert(config.server.certs.certificate, messageError('server.certs.certificate'))
     assert(config.server.certs.chain, messageError('server.certs.chain'))
   }
+
+  assert(config.authentification, messageError('authentification'))
+  assert(config.authentification.status === false || config.authentification.status, messageError('authentification.status'))
+
+  assert(config.database, messageError('database'))
+  assert(config.database.port, messageError('database.port'))
+  assert(config.database.host, messageError('database.host'))
 
   assert(config.client, messageError('client'))
   assert(config.client.downloads, messageError('client.downloads'))
